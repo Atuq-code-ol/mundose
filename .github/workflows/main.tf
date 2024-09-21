61bec58841733e712d75fd4ada8a654aec9a3fdb
Atuq-Code <atuq.code@outlook.com>
2024-09-21 17:34:13 -0500

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,4 +1,14 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+##Create and bootstrap webserver #lanzar
+resource "aws_instance" "webserver" {
+  ami                         = "ami-00c39f71452c08778" #actulializar
+  instance_type               = "t2.micro"
+  #key_name                    = app-ssh-key 
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = aws_subnet.subnet.id
+  user_data                   = "${file("create_apache.sh")}"
+
+  tags = {
+    Name = "webserver"
+  }
 }
