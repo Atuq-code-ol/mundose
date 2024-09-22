b5beae82e6b31ebfd265a14972b34a247b16acf3
Atuq-Code <atuq.code@outlook.com>
2024-09-21 21:51:59 -0500

    Update c_apache.sh

--- c_apache.sh
+++ c_apache.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
-sudo update -y
-sudo install -y httpd.x86_64
-sudo systemctl enable httpd --now
+# Atualiza a lista de pacotes
+sudo apt update
+
+# Instala o Apache em modo desatendido
+sudo apt install -y apache2
+
+# Reinicia o serviço Apache
+sudo systemctl restart apache2
+
+# Verifica se o serviço está ativo
+sudo systemctl status apache2
